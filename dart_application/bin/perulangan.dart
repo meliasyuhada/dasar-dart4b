3a34b4d89973e88916a2cda29ac28c34fb01bd9b
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 03:51:22 +0700

    edit perulngan

--- dart_application/bin/perulangan.dart
+++ dart_application/bin/perulangan.dart
@@ -2,17 +2,26 @@ import 'dart:io';
 
 
 main(List<String> arguments) {
-  int i = 0;
-  while(i < 5){
-    print("Helo ke " + i.toString());
-    i += 1;
-  }
+  int a, b;
+  a = 10;
+  b = ++a;
+  print(a.toString() + " - " + b.toString());
 
-  print("===========");
 
-  int x = 0;
-  do {
-    print("dododo ke" + x.toString());
-    x += 1;
-  } while (x < 5);
+
+
+
+  // int i = 0;
+  // while(i < 5){
+  //   print("Helo ke " + i.toString());
+  //   i += 1;
+  // }
+
+  // print("===========");
+
+  // int x = 0;
+  // do {
+  //   print("dododo ke" + x.toString());
+  //   x += 1;
+  // } while (x < 5);
 }
