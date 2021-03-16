f988ff33121ef79ff99b806ae62a80a252501ff5
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 02:50:05 +0700

    terakhir tambah

--- dart_application/test/dart_application_test.dart
+++ dart_application/test/dart_application_test.dart
@@ -4,12 +4,21 @@ import 'dart:io';
 void main(List<String> arguments) {
   int number = int.tryParse(stdin.readLineSync());
 
-  if (number > 0) {
-    print("Positif");
-  } else if (number < 0) {
-    print("negatif");
-  }else {
-    print("nol");
-  }
+  // if (number > 0) {
+  //   print("Positif");
+  // } else if (number < 0) {
+  //   print("negatif");
+  // }else {
+  //   print("nol");
+  // }
 
+  if (number == 0) {
+    print("nol");
+  } else if (number == 1) {
+    print("satu");
+  } else if (number == 2) {
+    print("dua");
+  } else {
+    print("bilangan lain");
+  }
 }
