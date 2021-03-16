72cb622781ba3acba16e793ba01128b6e979b26e
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 03:48:11 +0700

    edit

--- dart_application/bin/perulangan.dart
+++ dart_application/bin/perulangan.dart
@@ -2,7 +2,17 @@ import 'dart:io';
 
 
 main(List<String> arguments) {
-  for (int counter =9; counter < 1; counter == 2){
-    print("hello");
+  int i = 0;
+  while(i < 5){
+    print("Helo ke " + i.toString());
+    i += 1;
   }
+
+  print("===========");
+
+  int x = 0;
+  do {
+    print("dododo ke" + x.toString());
+    x += 1;
+  } while (x < 5);
 }
