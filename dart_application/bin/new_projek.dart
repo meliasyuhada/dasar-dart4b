c7bbd29d39a04d223b83daa337e4195876ee1fd9
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-18 09:46:15 +0700

    tambah

---
+++ dart_application/bin/new_projek.dart
@@ -0,0 +1,5 @@
+import 'dart:io';
+
+void main() {
+  print("Hello Dunia, Gimana kabarnya? ya kutahu gak ada yang namanya baik-baik saja, tapi jangan lupa bersyukur ya bagaimana pun Allah telah baik menciptakanmu dan memberikan kesempatanmu hidup di dunia ini walaupun sementara. jangan lupa bersyukur dan tersenyum, dan selalu ingat dunia sementara!!!");
+}
