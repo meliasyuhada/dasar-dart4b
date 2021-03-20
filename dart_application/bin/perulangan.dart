6efb8f5970bd9e32f535a2dad75ec74a4dbda9d9
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-20 21:03:23 +0700

    aktif

--- dart_application/bin/perulangan.dart
+++ dart_application/bin/perulangan.dart
@@ -2,26 +2,26 @@ import 'dart:io';
 
 
 main(List<String> arguments) {
-  int a, b;
-  a = 10;
-  b = ++a;
-  print(a.toString() + " - " + b.toString());
+  // int a, b;
+  // a = 10;
+  // b = ++a;
+  // print(a.toString() + " - " + b.toString());
 
 
 
 
 
-  // int i = 0;
-  // while(i < 5){
-  //   print("Helo ke " + i.toString());
-  //   i += 1;
-  // }
+  int i = 0;
+  while(i < 5){
+    print("Helo ke " + i.toString());
+    i += 1;
+  }
 
-  // print("===========");
+  print("===========");
 
-  // int x = 0;
-  // do {
-  //   print("dododo ke" + x.toString());
-  //   x += 1;
-  // } while (x < 5);
+  int x = 0;
+  do {
+    print("dododo ke" + x.toString());
+    x += 1;
+  } while (x < 5);
 }
